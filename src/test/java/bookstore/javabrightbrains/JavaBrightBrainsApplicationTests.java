7ddfd86be555c568ea6a9b13c17a717cbea758ae
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-10-03 22:13:28 +0200

    init project

---
+++ src/test/java/bookstore/javabrightbrains/JavaBrightBrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package bookstore.javabrightbrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBrightBrainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
