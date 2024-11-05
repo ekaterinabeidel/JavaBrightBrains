b832bcbf6ffe2e8411bcfe5c27f1923d90dd4d6b
Svetlana Zhuravleva <karpenter.xo@gmail.com>
2024-11-05 14:21:07 +0100

    BOOK-8 add summery for swagger

--- src/main/java/bookstore/javabrightbrains/controller/FavoriteController.java
+++ src/main/java/bookstore/javabrightbrains/controller/FavoriteController.java
@@ -3,6 +3,7 @@ package bookstore.javabrightbrains.controller;
 import bookstore.javabrightbrains.dto.book.BookShortResponseDto;
 import bookstore.javabrightbrains.dto.favorite.FavoriteRequestDto;
 import bookstore.javabrightbrains.service.FavoriteService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +18,14 @@ public class FavoriteController {
     @Autowired
     private FavoriteService favoriteServiceImpl;
 
+    @Operation(summary = "Get list of books that are in favorites")
     @GetMapping("/favorites/{userId}")
     public ResponseEntity<List<BookShortResponseDto>> getAllBooks(@PathVariable Long userId) {
         List<BookShortResponseDto> favorites = favoriteServiceImpl.getFavorites(userId);
         return ResponseEntity.ok(favorites);
     }
 
+    @Operation(summary = "Add book in favorites")
     @PostMapping("/favorites")
     public ResponseEntity<BookShortResponseDto> addFavorite(@RequestBody FavoriteRequestDto favorite) {
         BookShortResponseDto book = favoriteServiceImpl.saveFavorite(favorite);
@@ -30,6 +33,7 @@ public class FavoriteController {
         return ResponseEntity.ok(book);
     }
 
+    @Operation(summary = "Delete book from favorites")
     @DeleteMapping("/favorites")
     public ResponseEntity<Void> deleteFavorite(@RequestBody FavoriteRequestDto favorite) {
         favoriteServiceImpl.deleteFavorite(favorite);
