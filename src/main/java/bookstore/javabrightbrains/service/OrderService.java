954146b1036d67ac6cc3b667b7d99efd2d81dcd8
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-11-12 16:28:05 +0100

    BOOK-4 refactor MappingUtils

--- src/main/java/bookstore/javabrightbrains/service/OrderService.java
+++ src/main/java/bookstore/javabrightbrains/service/OrderService.java
@@ -1,6 +1,5 @@
 package bookstore.javabrightbrains.service;
 
-import bookstore.javabrightbrains.dto.book.BookOrderShortResponseDto;
 import bookstore.javabrightbrains.dto.order.OrderItemDto;
 import bookstore.javabrightbrains.dto.order.OrderRequestDto;
 import bookstore.javabrightbrains.dto.order.OrderResponseDto;
@@ -8,6 +7,7 @@ import bookstore.javabrightbrains.dto.order.OrderShortResponseDto;
 import bookstore.javabrightbrains.entity.*;
 import bookstore.javabrightbrains.exception.*;
 import bookstore.javabrightbrains.repository.*;
+import bookstore.javabrightbrains.utils.MappingUtils;
 import bookstore.javabrightbrains.utils.OrderStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +15,6 @@ import org.springframework.validation.annotation.Validated;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 
 @Validated
@@ -24,23 +23,21 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
     @Autowired
-    private BookRepository bookRepository;
-    @Autowired
     private CartRepository cartRepository;
     @Autowired
     private CartItemRepository cartItemRepository;
     @Autowired
-    private OrderItemRepository orderItemRepository;
-    @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private MappingUtils mappingUtils;
 
 
     public OrderResponseDto createOrder(OrderRequestDto orderRequestDto) {
 
         List<CartItem> cartItems = getValidatedCartItems(orderRequestDto.getCartId());
 
-        List<OrderItemDto> orderItems = convertCartItemsToOrderItems(cartItems);
-        BigDecimal totalPrice = calculateTotalPrice(orderItems);
+        List<OrderItemDto> orderItems = mappingUtils.convertCartItemsToOrderItems(cartItems);
+        BigDecimal totalPrice = mappingUtils.calculateTotalPrice(orderItems);
 
         Order order = createAndSaveOrder(orderRequestDto);
 
@@ -62,7 +59,7 @@ public class OrderService {
         Order order = orderRepository.findById(id)
                 .orElseThrow(() -> new IdNotFoundException(MessagesException.ORDER_NOT_FOUND));
 
-        return mapToOrderResponseDto(order);
+        return mappingUtils.mapToOrderResponseDto(order);
     }
 
     public List<OrderShortResponseDto> getOrdersByUserId(Long userId) {
@@ -73,9 +70,8 @@ public class OrderService {
 
         List<Order> orders = orderRepository.findByUserIdOrderByCreatedAtDesc(userId);
 
-        return orders.stream().map(this::mapToOrderShortResponseDto).toList();
+        return orders.stream().map(order -> mappingUtils.mapToOrderShortResponseDto(order)).toList();
     }
-
 
     public OrderShortResponseDto cancelOrder(Long orderId) {
         Order order = orderRepository.findById(orderId)
@@ -87,7 +83,7 @@ public class OrderService {
         order.setStatus(OrderStatus.CANCELED);
         orderRepository.save(order);
 
-        return mapToOrderShortResponseDto(order);
+        return mappingUtils.mapToOrderShortResponseDto(order);
     }
 
     private List<CartItem> getValidatedCartItems(Long cartId) {
@@ -101,38 +97,6 @@ public class OrderService {
         return cartItems;
     }
 
-    private List<OrderItemDto> convertCartItemsToOrderItems(List<CartItem> cartItems) {
-        List<OrderItemDto> orderItems = new ArrayList<>();
-        for (CartItem cartItem : cartItems) {
-            Book book = cartItem.getBook();
-            int quantity = cartItem.getQuantity();
-
-            if (book.getTotalStock() < quantity) {
-                throw new NotEnoughBooksInStockException(MessagesException.NOT_ENOUGH_BOOKS_IN_STOCK);
-            }
-
-            book.setTotalStock(book.getTotalStock() - quantity);
-            bookRepository.save(book);
-
-            double priceAtPurchase = book.getPrice().doubleValue();
-            orderItems.add(new OrderItemDto(
-                    cartItem.getId(),
-                    new BookOrderShortResponseDto(book.getId(), book.getTitle(), book.getAuthor()),
-                    quantity,
-                    priceAtPurchase
-            ));
-        }
-
-        return orderItems;
-    }
-
-    private BigDecimal calculateTotalPrice(List<OrderItemDto> orderItemsDto) {
-
-        return orderItemsDto.stream()
-                .map(item -> BigDecimal.valueOf(item.getPriceAtPurchase()).multiply(BigDecimal.valueOf(item.getQuantity())))
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
-
     private Order createAndSaveOrder(OrderRequestDto orderRequestDto) {
         Order order = new Order();
         order.setDeliveryAddress(orderRequestDto.getDeliveryAddress());
@@ -142,53 +106,5 @@ public class OrderService {
         order.setCreatedAt(new Timestamp(System.currentTimeMillis()));
 
         return orderRepository.save(order);
-    }
-
-    private OrderResponseDto mapToOrderResponseDto(Order order) {
-        List<OrderItem> orderItems = orderItemRepository.findByOrderId(order.getId());
-        List<OrderItemDto> orderItemsDto = mapToOrderItemsDto(orderItems);
-
-        BigDecimal totalPrice = calculateTotalPrice(orderItemsDto);
-
-        return new OrderResponseDto(
-                order.getId(),
-                orderItemsDto,
-                order.getCreatedAt(),
-                order.getDeliveryAddress(),
-                order.getContactPhone(),
-                order.getDeliveryMethod(),
-                order.getStatus(),
-                totalPrice
-        );
-    }
-    private OrderShortResponseDto mapToOrderShortResponseDto(Order order) {
-        List<OrderItem> orderItems = orderItemRepository.findByOrderId(order.getId());
-        List<OrderItemDto> orderItemsDto = mapToOrderItemsDto(orderItems);
-
-        BigDecimal totalPrice = calculateTotalPrice(orderItemsDto);
-
-        return new OrderShortResponseDto(
-                order.getId(),
-                orderItemsDto,
-                order.getCreatedAt(),
-                order.getStatus(),
-                totalPrice
-        );
-    }
-
-    private List<OrderItemDto> mapToOrderItemsDto(List<OrderItem> orderItems) {
-
-        return orderItems.stream()
-                .map(orderItem -> new OrderItemDto(
-                        orderItem.getId(),
-                        new BookOrderShortResponseDto(
-                                orderItem.getBook().getId(),
-                                orderItem.getBook().getTitle(),
-                                orderItem.getBook().getAuthor()
-                        ),
-                        orderItem.getQuantity(),
-                        orderItem.getPriceAtPurchase()
-                ))
-                .toList();
     }
 }
--- src/main/java/bookstore/javabrightbrains/utils/MappingUtils.java
+++ src/main/java/bookstore/javabrightbrains/utils/MappingUtils.java
@@ -1,21 +1,34 @@
 package bookstore.javabrightbrains.utils;
 
+import bookstore.javabrightbrains.dto.book.BookOrderShortResponseDto;
 import bookstore.javabrightbrains.dto.book.BookShortResponseDto;
 import bookstore.javabrightbrains.dto.cart.CartItemResponseDto;
 import bookstore.javabrightbrains.dto.cart.CartItemRequestDto;
 import bookstore.javabrightbrains.dto.cart.CartResponseDto;
-import bookstore.javabrightbrains.entity.Book;
-import bookstore.javabrightbrains.entity.Cart;
-import bookstore.javabrightbrains.entity.CartItem;
+import bookstore.javabrightbrains.dto.order.OrderItemDto;
+import bookstore.javabrightbrains.dto.order.OrderResponseDto;
+import bookstore.javabrightbrains.dto.order.OrderShortResponseDto;
+import bookstore.javabrightbrains.entity.*;
+import bookstore.javabrightbrains.exception.MessagesException;
+import bookstore.javabrightbrains.exception.NotEnoughBooksInStockException;
+import bookstore.javabrightbrains.repository.BookRepository;
+import bookstore.javabrightbrains.repository.OrderItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class MappingUtils {
+
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     public CartItem toCartItem(CartItemRequestDto cartItemRequestDto, Cart cart, Book book) {
         CartItem cartItem = new CartItem();
@@ -35,7 +48,7 @@ public class MappingUtils {
         return new CartResponseDto(cart.getId(), cartItemDtos);
     }
 
-    private CartItemResponseDto toCartItemResponseDto(CartItem cartItem) {
+    public CartItemResponseDto toCartItemResponseDto(CartItem cartItem) {
 
         BookShortResponseDto bookShortResponseDto = new BookShortResponseDto(
                 cartItem.getBook().getId(),
@@ -59,4 +72,84 @@ public class MappingUtils {
         );
     }
 
+    public List<OrderItemDto> convertCartItemsToOrderItems(List<CartItem> cartItems) {
+        List<OrderItemDto> orderItems = new ArrayList<>();
+        for (CartItem cartItem : cartItems) {
+            Book book = cartItem.getBook();
+            int quantity = cartItem.getQuantity();
+
+            if (book.getTotalStock() < quantity) {
+                throw new NotEnoughBooksInStockException(MessagesException.NOT_ENOUGH_BOOKS_IN_STOCK);
+            }
+
+            book.setTotalStock(book.getTotalStock() - quantity);
+            bookRepository.save(book);
+
+            double priceAtPurchase = book.getPrice().doubleValue();
+            orderItems.add(new OrderItemDto(
+                    cartItem.getId(),
+                    new BookOrderShortResponseDto(book.getId(), book.getTitle(), book.getAuthor()),
+                    quantity,
+                    priceAtPurchase
+            ));
+        }
+
+        return orderItems;
+    }
+
+    public List<OrderItemDto> mapToOrderItemsDto(List<OrderItem> orderItems) {
+
+        return orderItems.stream()
+                .map(orderItem -> new OrderItemDto(
+                        orderItem.getId(),
+                        new BookOrderShortResponseDto(
+                                orderItem.getBook().getId(),
+                                orderItem.getBook().getTitle(),
+                                orderItem.getBook().getAuthor()
+                        ),
+                        orderItem.getQuantity(),
+                        orderItem.getPriceAtPurchase()
+                ))
+                .toList();
+    }
+
+    public OrderResponseDto mapToOrderResponseDto(Order order) {
+        List<OrderItem> orderItems = orderItemRepository.findByOrderId(order.getId());
+        List<OrderItemDto> orderItemsDto = mapToOrderItemsDto(orderItems);
+
+        BigDecimal totalPrice = calculateTotalPrice(orderItemsDto);
+
+        return new OrderResponseDto(
+                order.getId(),
+                orderItemsDto,
+                order.getCreatedAt(),
+                order.getDeliveryAddress(),
+                order.getContactPhone(),
+                order.getDeliveryMethod(),
+                order.getStatus(),
+                totalPrice
+        );
+    }
+
+    public OrderShortResponseDto mapToOrderShortResponseDto(Order order) {
+        List<OrderItem> orderItems = orderItemRepository.findByOrderId(order.getId());
+        List<OrderItemDto> orderItemsDto = mapToOrderItemsDto(orderItems);
+
+        BigDecimal totalPrice = calculateTotalPrice(orderItemsDto);
+
+        return new OrderShortResponseDto(
+                order.getId(),
+                orderItemsDto,
+                order.getCreatedAt(),
+                order.getStatus(),
+                totalPrice
+        );
+    }
+
+    public BigDecimal calculateTotalPrice(List<OrderItemDto> orderItemsDto) {
+
+        return orderItemsDto.stream()
+                .map(item -> BigDecimal.valueOf(item.getPriceAtPurchase()).multiply(BigDecimal.valueOf(item.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 }
