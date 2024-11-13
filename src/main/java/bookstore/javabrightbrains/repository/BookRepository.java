00521a47094ff414a76f65e4bc893fb7ee902812
ivanbeidel <ivanbeidel@gmail.com>
2024-11-13 18:34:44 +0100

    BOOK-13 deleted @Query

--- src/main/java/bookstore/javabrightbrains/repository/BookRepository.java
+++ src/main/java/bookstore/javabrightbrains/repository/BookRepository.java
@@ -2,11 +2,10 @@ package bookstore.javabrightbrains.repository;
 
 import bookstore.javabrightbrains.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 
 public interface BookRepository extends JpaRepository<Book, Long>, FilterBookRepository {
-    @Query("SELECT b FROM Book b WHERE b.discount = (SELECT MAX(b2.discount) FROM Book b2)")
     List<Book> findFirstByOrderByDiscountDesc();
 }
