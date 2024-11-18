6fa2fd57404d4f2a61e9edb38f972eeb6753eefb
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-11-18 14:00:25 +0100

    BOOK-19 update user dto, all fields are required

--- src/main/java/bookstore/javabrightbrains/dto/user/UserDto.java
+++ src/main/java/bookstore/javabrightbrains/dto/user/UserDto.java
@@ -1,21 +1,23 @@
 package bookstore.javabrightbrains.dto.user;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class UserDto {
+    @NotNull(message = "Name is required")
     @Size(min = 1, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
 
+    @NotNull(message = "Surname is required")
     @Size(min = 1, max = 50, message = "Name must be between 2 and 50 characters")
     private String surname;
 
+    @NotNull(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
 
+    @NotNull(message = "Phone number is required")
     @Pattern(regexp = "^(\\+\\d{1,3}[- ]?)?\\d{10}$", message = "Phone number is invalid")
     private String phone;
 }
