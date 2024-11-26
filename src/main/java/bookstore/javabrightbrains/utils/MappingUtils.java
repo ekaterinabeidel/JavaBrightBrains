17111e1ba761cb10cb45132ed24faff431949ab6
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-11-26 17:03:21 +0100

    BOOK-19 merge development into branch, add mapping utils

--- src/main/java/bookstore/javabrightbrains/utils/MappingUtils.java
+++ src/main/java/bookstore/javabrightbrains/utils/MappingUtils.java
@@ -232,3 +232,86 @@ public class MappingUtils {
             book.setPriceDiscount(getPriceWithDiscount(bookRequestDto.getPrice(), bookRequestDto.getDiscount()));
         }
 
+        book.setTotalStock(bookRequestDto.getTotalStock());
+        book.setImageLink(bookRequestDto.getImageLink());
+
+        Long categoryId = bookRequestDto.getCategoryId();
+        if (categoryId != null) {
+            Category category = categoryService.findEntityById(categoryId);
+            book.setCategory(category);
+        }
+        return book;
+    }
+
+    public BigDecimal getPriceWithDiscount(BigDecimal price, int discount) {
+        BigDecimal discountAmount = price
+                .multiply(BigDecimal.valueOf(discount))
+                .divide(BigDecimal.valueOf(100), RoundingMode.HALF_UP);
+        return price.subtract(discountAmount);
+    }
+
+    public BookResponseDto convertToBookResponseDto(Book book) {
+        BookResponseDto bookDto = new BookResponseDto();
+        bookDto.setId(book.getId());
+        bookDto.setTitle(book.getTitle());
+        bookDto.setAuthor(book.getAuthor());
+        bookDto.setDescription(book.getDescription());
+        bookDto.setPrice(book.getPrice());
+        bookDto.setDiscount(book.getDiscount());
+        bookDto.setTotalStock(book.getTotalStock());
+        bookDto.setImageLink(book.getImageLink());
+        bookDto.setPriceDiscount(book.getPriceDiscount());
+
+        Category category = book.getCategory();
+
+        if (category != null) {
+            bookDto.setCategoryId(category.getId());
+        }
+
+        return bookDto;
+    }
+
+    public BookShortResponseDto convertToBookShortResponseDto(Book book) {
+        BookShortResponseDto bookDto = new BookShortResponseDto();
+        bookDto.setId(book.getId());
+        bookDto.setTitle(book.getTitle());
+        bookDto.setAuthor(bookDto.getAuthor());
+        bookDto.setPrice(book.getPrice());
+        bookDto.setDiscount(book.getDiscount());
+        bookDto.setTotalStock(book.getTotalStock());
+        bookDto.setImageLink(book.getImageLink());
+        bookDto.setPriceDiscount(book.getPriceDiscount());
+
+        Category category = book.getCategory();
+        if (category != null) {
+            bookDto.setCategoryId(category.getId());
+        }
+
+        return bookDto;
+    }
+
+    public Book updateBookFromDto(Book book, BookRequestDto bookDto) {
+        if (bookDto.getTitle() != null) book.setTitle(bookDto.getTitle());
+        if (bookDto.getAuthor() != null) book.setAuthor(bookDto.getAuthor());
+        if (bookDto.getDescription() != null) book.setDescription(bookDto.getDescription());
+        if (bookDto.getPrice() != null) book.setPrice(bookDto.getPrice());
+
+        book.setDiscount(bookDto.getDiscount());
+        if (bookDto.getDiscount() > 0) {
+            book.setPriceDiscount(getPriceWithDiscount(bookDto.getPrice(), bookDto.getDiscount()));
+        } else {
+            book.setPriceDiscount(bookDto.getPrice());
+        }
+
+        if (bookDto.getTotalStock() != 0) book.setTotalStock(bookDto.getTotalStock());
+        if (bookDto.getImageLink() != null) book.setImageLink(bookDto.getImageLink());
+
+        Long categoryId = bookDto.getCategoryId();
+        if (categoryId != null) {
+            Category category = categoryService.findEntityById(categoryId);
+            book.setCategory(category);
+        }
+        return book;
+    }
+
+}
